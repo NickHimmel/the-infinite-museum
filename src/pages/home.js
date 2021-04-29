dcf5a7007ffdf41082884538fbb464e4ca02418c
NickHimmel <himmel.nick@gmail.com>
2021-04-29 17:04:56 -0400

    Add home page component

---
+++ src/pages/home.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from "react";
+
+function Home() {
+    useEffect(() => {
+        document.title = "The Infinite Museum";
+    }, []);
+
+    return (
+        <div>
+            
+        </div>
+    )
+};
+
+export default Home;
