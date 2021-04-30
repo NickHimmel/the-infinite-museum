86eb797801fa12f2ab52b0585428224bcf9f287f
NickHimmel <himmel.nick@gmail.com>
2021-04-30 11:32:59 -0400

    Add main headers

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,13 @@
 import React from "react";
 
 function Home() {
-    
+
     return (
         <div>
-
+            <h2>Explore Open Access At The Met</h2>
+            <h2>Find Your Favorite Artworks</h2>
+            <h2>Curate Exhibitions And Share</h2>
+            <h1>The Infinite Museum</h1>
         </div>
     )
 };
