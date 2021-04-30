5e567587a5a029160836f7a50e40bfda7b13e7f1
NickHimmel <himmel.nick@gmail.com>
2021-04-30 19:56:46 -0400

    Add nav bar component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import {
   Route
 } from 'react-router-dom';
 import Home from './pages/home'; 
+import Nav from './components/nav'; 
 import * as ROUTES from './constants/routes';
 import './App.css';
 
 function App() {
   return (
     <Router>
+      <Nav />
       <Route path={ROUTES.HOME} component={Home}/>
     </Router>
   );
---
+++ src/components/nav.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Nav() {
+    return(
+        <nav>
+            <h3>IM</h3>
+            <h4>The Met Musuem</h4>
+            <ul>
+                <li><h4>Find Art</h4></li>
+                <li><h4>Exhibitions</h4></li>
+                <li><h4>Create Account / Login</h4></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav;
